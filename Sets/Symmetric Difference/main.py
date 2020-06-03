40a5e602c1fbed9e6014c4222897425448e50098
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-06-03 12:12:56 +0530

    Create a short version of Symmetric difference

--- Sets/Symmetric Difference/main.py
+++ Sets/Symmetric Difference/main.py
@@ -11,3 +11,7 @@ if __name__ == '__main__':
     input()
     n_ = set(map(int, input().split()))
     symmetric_difference(m_, n_)
+
+# Short version
+# m, n = map(set, [map(int, input().strip().split()) for _ in range(4)][1:4:2])
+# print("\n".join(map(str, sorted([i for i in m if i not in n] + [i for i in n if i not in m]))))
