e4faac6d45a16adf48b132b9feaa2e48afc3710f
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-07-18 17:09:02 +0530

    Fix Group(), Groups() & Groupdict()

--- Regex and Parsing/Group(), Groups() & Groupdict()/main.py
+++ Regex and Parsing/Group(), Groups() & Groupdict()/main.py
@@ -2,8 +2,9 @@ import re
 
 if __name__ == '__main__':
     _input = input()
-    pattern = r'(\w)\1'
+    pattern = r'([0-1a-zA-Z])\1'
     matcher = re.search(pattern, _input)
+
     if matcher:
         print(matcher.group(1))
     else:
